c96dba12433108148f05a20df88dd14e6a5bee93
Muhammad-Abdullah-Programmer <124166045+Muhammad-Abdullah-Programmer@users.noreply.github.com>
2023-08-01 20:43:22 +0500

    Update intents.py

--- intents.py
+++ intents.py
@@ -2,4 +2,4 @@ import datetime
 day = int(datetime.datetime.now().strftime("%d"))
 month = int(datetime.datetime.now().strftime("%m"))
 year = int(datetime.datetime.now().strftime("%y"))
-ver = 2.1
+ver = 2.4
