3e8a380ab047e122cbf925992877b2b24edf8dc8
Muhammad-Abdullah-Programmer <124166045+Muhammad-Abdullah-Programmer@users.noreply.github.com>
2023-08-01 20:40:21 +0500

    Delete v2.4 directory

--- v2.4/replace.py
+++
@@ -1,8 +0,0 @@
-def replace(one, two):
-    file = open('intents.py', "r")
-    data = file.read()
-    rdata = data.replace(one, two)
-    file.close()
-    fin = open('intents.py', "w")
-    fin.write(rdata)
-    fin.close()
