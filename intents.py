65cfaf732006c87fe3c0e2e6389a2cd7e73da5fd
Muhammad-Abdullah-Programmer <124166045+Muhammad-Abdullah-Programmer@users.noreply.github.com>
2023-08-01 20:24:35 +0500

    Delete intents.py

--- intents.py
+++ intents.py
@@ -1,5 +1,1 @@
-import datetime
-day = int(datetime.datetime.now().strftime("%d"))
-month = int(datetime.datetime.now().strftime("%m"))
-year = int(datetime.datetime.now().strftime("%y"))
-ver = 2.1
+
